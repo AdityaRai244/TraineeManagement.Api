acdc336d75dfebcedc45078d07ec05a2fe2de904
AdityaRai244 <aditya.rai@zeuslearning.com>
2026-07-30 14:20:54 +0530

    added system.security to claims

--- TraineeManagement.Api/Program.cs
+++ TraineeManagement.Api/Program.cs
@@ -167,7 +167,7 @@ builder.Services.AddAuthentication(options =>
                 ValidIssuer = builder.Configuration["Jwt:Issuer"],
                 ValidAudience = builder.Configuration["Jwt:Audience"],
                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!)),
-                RoleClaimType = Claims.ClaimTypes.Role
+                RoleClaimType = System.Security.Claims.ClaimTypes.Role
             };
         });
 
